3c3274ab63eb9b17a120b140d6b33fea095c24ad
Anita <smirnovasiberia@gmail.com>
2019-10-02 14:53:00 +0300

    LoginHandler done.

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -10,8 +10,7 @@ import (
 	"net/http"
 )
 
-// curl --data "password=1234&username=Anita" http://localhost:3000/user/login
-//
+//  curl -XPOST -H "Content-type: application/json" -d '{"username": "anita", "password":"1234", "email":"anit@mail.com"}' 'http://localhost:3000/user/login'
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	u := entity.User{}
@@ -19,47 +18,20 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	//b, err := ioutil.ReadAll(r.Body)
-	//fmt.Println(b)
-	//defer r.Body.Close()
-	//
-	//if err != nil {
-	//	http.Error(w, err.Error(), 500)
-	//	return
-	//}
-	//
-	//u := entity.User{}
-	//err = json.Unmarshal(b, &u)
-	//if err != nil {
-	//	http.Error(w, err.Error(), 500)
-	//	return
-	//}
-	//fmt.Fprintln(w, u)
-
-	//output, err := json.Marshal(u)
-	//if err != nil {
-	//	http.Error(w, err.Error(), 500)
-	//	return
-	//}
-	//w.Header().Set("content-type", "application/json")
-	//w.Write(output)
-}
-
-// curl --data "password=1234&username=Anita&email=heh@mail.ru" http://localhost:3000/user/signup
-func SignupHandler(w http.ResponseWriter, r *http.Request) {
-	inputUsername := r.FormValue("username")
-	inputPassword := r.FormValue("password")
-	inputEmail := r.FormValue("email")
-	createdUser, err := repository.Data.UserCreate(inputUsername, inputPassword, inputEmail)
+	createdUser, err := repository.Data.UserCreate(u.Username, u.Password, u.Email)
 	if err != nil {
 		fmt.Fprintln(w, "creation error")
 	}
 	fmt.Fprintln(w, createdUser)
 }
+
+func SignupHandler(w http.ResponseWriter, r *http.Request) {
+}
+
 func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 }
 
-func SetProfile(w http.ResponseWriter, r *http.Request) {
+func GetProfileHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateProfile(w http.ResponseWriter, r *http.Request) {
@@ -69,7 +41,7 @@ func Start() {
 	r := mux.NewRouter()
 	r.HandleFunc("/user/login", LoginHandler).Methods("POST")
 	r.HandleFunc("/user/signup", SignupHandler).Methods("POST")
-	r.HandleFunc("/user", SetProfile).Methods("GET")
+	r.HandleFunc("/user", GetProfileHandler).Methods("GET")
 	r.HandleFunc("/user", UpdateProfile).Methods("PUT")
 	r.HandleFunc("/user", LogoutHandler).Methods("DELETE")
 
