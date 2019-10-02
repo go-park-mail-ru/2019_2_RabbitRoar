051f032db3513a51ba4247ea70887528d0f18c29
Egor <egor140398@gmail.com>
2019-10-02 17:41:15 +0300

    Create UpdateProfile handler.

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -8,6 +8,7 @@ import (
 	"../../../cmd/entity"
 	"../../../cmd/repository"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/context"
 	"github.com/google/uuid"
 )
 
@@ -56,6 +57,44 @@ func GetProfileHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateProfile(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var mainUser entity.User
+	mainUser = context.Get(r, "user").(entity.User)
+	
+	userUpdated := &entity.User{}
+
+	if userFromBody, err := ioutil.ReadAll(r.Body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	} else {
+		if err := json.Unmarshal(userFromBody, userUpdated); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	changes := 0
+	if userUpdated.Email != "" {
+		changes++
+		mainUser.Email = userUpdated.Email
+	}
+	if userUpdated.Url != "" {
+		changes++
+		mainUser.Url = userUpdated.Url
+	}
+
+	if changes == 0 {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+
+	if err := repository.Data.UserUpdate(mainUser); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func Start() {
