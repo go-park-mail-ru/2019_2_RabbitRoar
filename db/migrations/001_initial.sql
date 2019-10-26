d4d51c1e70ac110207aefa30b215000536501539
Alexey Navarkin <navarka.all@gmail.com>
2019-10-26 13:14:36 +0300

    Update migration.

--- db/migrations/001_initial.sql
+++ db/migrations/001_initial.sql
@@ -14,8 +14,6 @@ CREATE TABLE IF NOT EXISTS "svoyak"."User" (
   "rating" INT NOT NULL,
   "avatar" VARCHAR(45) NULL,
   PRIMARY KEY ("id"));
---   INDEX "username_idx" ("username" ASC) VISIBLE,
---   INDEX "email_idx" ("email" ASC) VISIBLE);
 
 
 -----------------------------------------------------
@@ -25,8 +23,6 @@ CREATE TABLE IF NOT EXISTS "svoyak"."Session" (
   "UUID" INT NOT NULL,
   "User_id" INT NOT NULL,
   PRIMARY KEY ("UUID"),
---   UNIQUE INDEX "UUID_UNIQUE" ("UUID" ASC) VISIBLE,
---   INDEX "fk_Session_User_idx" ("User_id" ASC) VISIBLE,
   CONSTRAINT "fk_Session_User"
     FOREIGN KEY ("User_id")
     REFERENCES "svoyak"."User" ("id")
@@ -47,9 +43,6 @@ CREATE TABLE IF NOT EXISTS "svoyak"."Question" (
   "author" INT NOT NULL,
   "tags" VARCHAR(128) NULL,
   PRIMARY KEY ("id"),
---   INDEX "fk_Question_QuestionType1_idx" ("type_id" ASC) VISIBLE,
---   INDEX "fk_Question_User1_idx" ("author" ASC) VISIBLE,
---   FULLTEXT INDEX "tags_fulltext_idx" ("tags") VISIBLE,
   CONSTRAINT "fk_Question_User"
     FOREIGN KEY ("author")
     REFERENCES "svoyak"."User" ("id")
@@ -70,10 +63,6 @@ CREATE TABLE IF NOT EXISTS "svoyak"."QuestionPack" (
   "private" BOOLEAN NOT NULL,
   "tags" VARCHAR(256) NULL,
   PRIMARY KEY ("id"),
---   INDEX "fk_QuestionPack_User1_idx" ("author" ASC) VISIBLE,
---   INDEX "name_idx" ("name" ASC) VISIBLE,
---   INDEX "author_id" ("author" ASC) VISIBLE,
---   FULLTEXT INDEX "tags_fulltext_idx" () VISIBLE,
   CONSTRAINT "fk_QuestionPack_User"
     FOREIGN KEY ("author")
     REFERENCES "svoyak"."User" ("id")
@@ -88,8 +77,6 @@ CREATE TABLE IF NOT EXISTS "svoyak"."PackQuestion" (
   "QuestionPack_id" INT NOT NULL,
   "Question_id" INT NOT NULL,
   PRIMARY KEY ("QuestionPack_id", "Question_id"),
---   INDEX "fk_QuestionPack_has_Question_Question1_idx" ("Question_id" ASC) VISIBLE,
---   INDEX "fk_QuestionPack_has_Question_QuestionPack1_idx" ("QuestionPack_id" ASC) VISIBLE,
   CONSTRAINT "fk_QuestionPack_Question"
     FOREIGN KEY ("QuestionPack_id")
     REFERENCES "svoyak"."QuestionPack" ("id")
@@ -113,10 +100,6 @@ CREATE TABLE IF NOT EXISTS "svoyak"."Game" (
   "creator" INT NOT NULL,
   "QuestionPack_id" INT NOT NULL,
   PRIMARY KEY ("UUID"),
---   INDEX "fk_Game_GameState1_idx" ("GameState_id" ASC) VISIBLE,
---   INDEX "fk_Game_User1_idx" ("creator" ASC) VISIBLE,
---   INDEX "fk_Game_PackQuestion1_idx" ("PackQuestion_QuestionPack_id" ASC, "PackQuestion_Question_id" ASC) VISIBLE,
---   INDEX "name_idx" ("name" ASC) VISIBLE,
   CONSTRAINT "fk_Game_User"
     FOREIGN KEY ("creator")
     REFERENCES "svoyak"."User" ("id")
@@ -136,8 +119,6 @@ CREATE TABLE IF NOT EXISTS "svoyak"."GameUser" (
   "User_id" INT NOT NULL,
   "Game_UUID" VARCHAR(45) NOT NULL,
   PRIMARY KEY ("User_id", "Game_UUID"),
---   INDEX "fk_User_has_Game_Game1_idx" ("Game_UUID" ASC) VISIBLE,
---   INDEX "fk_User_has_Game_User1_idx" ("User_id" ASC) VISIBLE,
   CONSTRAINT "fk_User_Game"
     FOREIGN KEY ("User_id")
     REFERENCES "svoyak"."User" ("id")
