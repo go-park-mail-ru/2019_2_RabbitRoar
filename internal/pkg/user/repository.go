158e8bae05779b9d13c4ddbc21f983b83c363fc8
Babich <g8h2.babayan@yandex.ru>
2019-10-28 02:11:53 +0300

    Yet another minor style change.

--- internal/pkg/user/repository.go
+++ internal/pkg/user/repository.go
@@ -3,8 +3,8 @@ package user
 import "github.com/go-park-mail-ru/2019_2_RabbitRoar/internal/pkg/models"
 
 type Repository interface {
-	GetByID(int) (*models.User, error)
-	GetByName(string) (*models.User, error)
+	GetByID(userID int) (*models.User, error)
+	GetByName(name string) (*models.User, error)
 	Create(user models.User) (*models.User, error)
-	Update(models.User) error
+	Update(user models.User) error
 }
