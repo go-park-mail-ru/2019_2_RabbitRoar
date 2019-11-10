4385498bcbb0d4e9d78ed21ed5cbfa1ccf4ae4d6
Alexey Navarkin <navarka.all@gmail.com>
2019-11-10 23:24:12 +0300

    Add RID and time for logger.

--- internal/pkg/middleware/log.go
+++ internal/pkg/middleware/log.go
@@ -3,14 +3,33 @@ package middleware
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/op/go-logging"
+	"math/rand"
+	"time"
 )
 
 var logLog = logging.MustGetLogger("middleware_log")
 
 func LogMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
-		logLog.Infof("%s %s", ctx.Request().Method, ctx.Request().RequestURI)
-		return next(ctx)
+		RID := rand.Int()
+		now := time.Now().Nanosecond()
+		ctx.Set("RID", RID)
+		logLog.Infof(
+			"RID%d pre %s %s",
+			RID,
+			ctx.Request().Method,
+			ctx.Request().RequestURI,
+		)
+		err := next(ctx)
+		logLog.Infof(
+			"RID%d T%dns %s %s",
+			RID,
+			now - time.Now().Nanosecond(),
+			ctx.Request().Method,
+			ctx.Response().Status,
+			ctx.Request().RequestURI,
+		)
+		return err
 	}
 }
 
