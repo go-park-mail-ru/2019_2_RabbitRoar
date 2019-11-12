919891cc422070f1c22124e816a7df20ab057049
Babich <g8h2.babayan@yandex.ru>
2019-11-12 16:23:08 +0300

    Game events implemented.

--- internal/pkg/game/event.go
+++ internal/pkg/game/event.go
@@ -1,6 +1,44 @@
 package game
 
+type EventType string
+
+const (
+	GameStart         EventType = "start_game"
+	RequestQuestion   EventType = "request_question_from_player"
+	QuestionChosen    EventType = "question_chosen"
+	RequestRespondent EventType = "request_respondent"
+	RespondentReady   EventType = "respondent_ready"
+	RequestAnswer     EventType = "request_answer_from_respondent"
+	AnswerGiven       EventType = "respondent_answer_given"
+	AnswerGivenBack   EventType = "answer_given_back"
+	RequestVerdict    EventType = "request_verdict_from_host"
+	VerdictCorrect    EventType = "verdict_correct"
+	VerdictWrong      EventType = "verdict_wrong"
+	VerictGivenBack   EventType = "verdict_given_back"
+)
+
 type Event struct {
-	Type string
+	Type    EventType
 	Payload interface{}
+}
+
+type GameStartPayload struct {
+	Themes [5]string
+}
+
+type RequestFromPlayerPayload struct {
+	PlayerID int
+}
+
+type QuestionChosenPayload struct {
+	Theme       int
+	QuestionIdx int
+}
+
+type AnswerPayload struct {
+	Answer string
+}
+
+type VerictPayload struct {
+	Verdict bool
 }
--- internal/pkg/game/event/event.go
+++ internal/pkg/game/event/event.go
@@ -1,5 +1,54 @@
 package event
 
-type Update struct {
+import "github.com/go-park-mail-ru/2019_2_RabbitRoar/internal/pkg/game"
 
+func NewEvent(et game.EventType, data ...interface{}) *game.Event {
+	e := game.Event{
+		Type: et,
+	}
+
+	switch et {
+	case game.GameStart:
+		e.Payload = game.GameStartPayload{
+			Themes: data[0].([5]string),
+		}
+
+	case game.RequestQuestion:
+		e.Payload = game.RequestFromPlayerPayload{
+			PlayerID: data[0].(int),
+		}
+
+	case game.QuestionChosen:
+		e.Payload = game.QuestionChosenPayload{
+			Theme:       data[0].(int),
+			QuestionIdx: data[1].(int),
+		}
+
+	case game.RequestAnswer:
+		e.Payload = game.RequestFromPlayerPayload{
+			PlayerID: data[0].(int),
+		}
+
+	case game.AnswerGiven:
+		e.Payload = game.AnswerPayload{
+			Answer: data[0].(string),
+		}
+
+	case game.AnswerGivenBack:
+		e.Payload = game.AnswerPayload{
+			Answer: data[0].(string),
+		}
+
+	case game.RequestVerdict:
+		e.Payload = game.RequestFromPlayerPayload{
+			PlayerID: data[0].(int),
+		}
+
+	case game.VerictGivenBack:
+		e.Payload = game.VerictPayload{
+			Verdict: data[0].(bool),
+		}
+	}
+
+	return &e
 }
