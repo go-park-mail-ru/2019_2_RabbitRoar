9b45490a80faaceb2900ec7d0cc120d737c87c93
Alexey Navarkin <navarka.all@gmail.com>
2019-11-12 15:16:45 +0300

    Update initial migration.

--- db/migrations/001_initial.sql
+++ db/migrations/001_initial.sql
@@ -3,19 +3,13 @@ CREATE SCHEMA IF NOT EXISTS "svoyak";
 
 CREATE TABLE IF NOT EXISTS "svoyak"."User"
 (
-    "id"       SERIAL,
-    "username" VARCHAR(45)  NOT NULL,
+    "id"       SERIAL       NOT NULL UNIQUE,
+    "username" VARCHAR(45)  NOT NULL UNIQUE,
     "password" bytea        NOT NULL,
-    "email"    VARCHAR(45)  NOT NULL,
+    "email"    VARCHAR(45)  NOT NULL UNIQUE,
     "rating"   INT          NOT NULL,
     "avatar"   VARCHAR(128) NULL,
-    "Game_id"  INT,
-    PRIMARY KEY ("id"),
-    CONSTRAINT "fk_User_Game"
-        FOREIGN KEY ("Game_id")
-            REFERENCES "svoyak"."Game" ("UUID")
-            ON DELETE SET NULL
-            ON UPDATE NO ACTION
+    PRIMARY KEY ("id")
 );
 
 
@@ -34,13 +28,13 @@ CREATE TABLE IF NOT EXISTS "svoyak"."Session"
 
 CREATE TABLE IF NOT EXISTS "svoyak"."Pack"
 (
-    "id"          INT          NOT NULL,
-    "name"        VARCHAR(45)  NOT NULL,
-    "author"      INT          NOT NULL,
-    "rating"      INT          NOT NULL,
-    "description" text         NOT NULL,
-    "tags"        VARCHAR(256) NOT NULL,
-    "pack"        json         NOT NULL,
+    "id"          SERIAL        NOT NULL,
+    "name"        VARCHAR(45)   NOT NULL,
+    "author"      INT           NOT NULL,
+    "rating"      INT DEFAULT 0 NOT NULL,
+    "description" text          NOT NULL,
+    "tags"        VARCHAR(256)  NOT NULL,
+    "pack"        json          NOT NULL,
     PRIMARY KEY ("id"),
     CONSTRAINT "fk_QuestionPack_User"
         FOREIGN KEY ("author")
@@ -52,12 +46,12 @@ CREATE TABLE IF NOT EXISTS "svoyak"."Pack"
 
 CREATE TABLE IF NOT EXISTS "svoyak"."Game"
 (
-    "UUID"           VARCHAR(45) NOT NULL,
-    "name"           VARCHAR(45) NOT NULL,
-    "players_cap"    SMALLINT    NOT NULL,
-    "players_joined" SMALLINT    NOT NULL,
-    "creator"        INT         NOT NULL,
-    "Pack_id"        INT         NOT NULL,
+    "UUID"           VARCHAR(45)        NOT NULL UNIQUE,
+    "name"           VARCHAR(45)        NOT NULL,
+    "players_cap"    SMALLINT           NOT NULL,
+    "players_joined" SMALLINT DEFAULT 0 NOT NULL,
+    "creator"        INT                NOT NULL,
+    "Pack_id"        INT                NOT NULL,
     PRIMARY KEY ("UUID"),
     CONSTRAINT "fk_Game_User"
         FOREIGN KEY ("creator")
@@ -71,22 +65,39 @@ CREATE TABLE IF NOT EXISTS "svoyak"."Game"
             ON UPDATE NO ACTION
 );
 
+CREATE TABLE IF NOT EXISTS "svoyak"."GameUser"
+(
+    "User_id"   INT         NOT NULL UNIQUE,
+    "Game_UUID" VARCHAR(45) NOT NULL,
+    PRIMARY KEY ("User_id", "Game_UUID"),
+    CONSTRAINT "fk_GameUser_User_id"
+        FOREIGN KEY ("User_id")
+            REFERENCES "svoyak"."User" ("id")
+            ON DELETE CASCADE
+            ON UPDATE NO ACTION,
+    CONSTRAINT "fk_GameUser_Game_id"
+        FOREIGN KEY ("Game_UUID")
+            REFERENCES "svoyak"."Game" ("UUID")
+            ON DELETE CASCADE
+            ON UPDATE NO ACTION
+);
+
 
 CREATE TABLE IF NOT EXISTS "svoyak"."UserPack"
 (
     "User_id" INT NOT NULL,
-    "Pack_id" INT NOT NULL,
+    "Pack_id" INT NOT NULL UNIQUE,
     PRIMARY KEY ("User_id", "Pack_id"),
     CONSTRAINT "fk_UserPack_User"
         FOREIGN KEY ("User_id")
             REFERENCES "svoyak"."User" ("id")
             ON DELETE NO ACTION
             ON UPDATE NO ACTION,
-    CONSTRAINT "fk_UserPack_Pack",
-    FOREIGN KEY ("Pack_id")
-        REFERENCES svoyak."Pack" ("id")
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+    CONSTRAINT "fk_UserPack_Pack"
+        FOREIGN KEY ("Pack_id")
+            REFERENCES svoyak."Pack" ("id")
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
 );
 
 
