a70bcac46cd62ddf517d10bbe7d2bea11fba1a4a
Babich <g8h2.babayan@yandex.ru>
2019-11-12 17:30:56 +0300

    Game handler self route implemented.

--- internal/pkg/game/delivery/http/game_handler.go
+++ internal/pkg/game/delivery/http/game_handler.go
@@ -1,46 +1,129 @@
 package http
 
 import (
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/go-park-mail-ru/2019_2_RabbitRoar/internal/pkg/game"
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
 )
 
-type handler struct {}
+type handler struct {
+	usecase game.UseCase
+}
 
 var upgrader = websocket.Upgrader{}
 
 func NewGameHandler(
 	e *echo.Echo,
+	uc game.UseCase,
 	authMiddleware echo.MiddlewareFunc,
 ) {
-	handler := handler{}
+	handler := handler{
+		usecase: uc,
+	}
 
 	group := e.Group("/game")
+	group.GET("/", handler.self)
+	// group.POST("/")
+	// group.POST("/:uuid/join")
+	// group.DELETE("/leave")
 	group.GET("/ws", handler.ws)
 }
 
-func (h *handler) ws(ctx echo.Context) error {
+func (gh *handler) self(ctx echo.Context) error {
+	page, err := strconv.Atoi(ctx.QueryParam("page"))
+	if err != nil {
+		return &echo.HTTPError{
+			Code:     http.StatusBadRequest,
+			Message:  "cannot parse page",
+			Internal: err,
+		}
+	}
+
+	if page < 0 {
+		return echo.NewHTTPError(http.StatusBadRequest, "page less than 0 provided")
+	}
+
+	content, err := gh.usecase.Fetch(page)
+	if err != nil {
+		return &echo.HTTPError{
+			Code:     http.StatusBadRequest,
+			Message:  "error fetching page of games",
+			Internal: err,
+		}
+	}
+
+	return ctx.JSON(http.StatusOK, content)
+}
+
+func (gh *handler) ws(ctx echo.Context) error {
 	ws, err := upgrader.Upgrade(ctx.Response(), ctx.Request(), nil)
 	if err != nil {
 		return err
 	}
 	defer ws.Close()
 
-	for {
-		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
-		if err != nil {
-			log.Error(err)
-		}
+	var wg sync.WaitGroup
 
-		_, msg, err := ws.ReadMessage()
-		if err != nil {
-			log.Error(err)
-		}
+	wg.Add(2)
 
-		err = ws.WriteMessage(websocket.TextMessage, msg)
-		if err != nil {
-			log.Error(err)
+	go func(readChan chan []byte, stop chan bool) {
+		defer wg.Done()
+
+		for {
+			select {
+			case <-stop:
+				close(readChan)
+				log.Info("Stopped reading from websocket manually")
+				return
+
+			default:
+				_, msg, err := ws.ReadMessage()
+				if err != nil {
+					close(readChan)
+					log.Info(err)
+					return
+				}
+
+				readChan <- msg
+			}
 		}
-	}
+	}()
+
+	go func(writeChan chan []byte, stop chan bool) {
+		defer wg.Done()
+
+		ticker := time.NewTicker(10 * time.Second)
+
+		for {
+			select {
+			case <-stop:
+				log.Info("Stopped writing into websocket manually")
+				return
+
+			case msg := <-writeChan:
+				err = ws.WriteMessage(websocket.TextMessage, msg)
+				if err != nil {
+					log.Info(err)
+					return
+				}
+
+			case <-ticker.C:
+				err := ws.WriteMessage(websocket.PingMessage, []byte{})
+				if err != nil {
+					log.Info(err)
+					return
+				}
+			}
+		}
+	}()
+
+	wg.Wait()
+
+	return nil
 }
