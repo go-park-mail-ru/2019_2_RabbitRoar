47e87157836c0ca755a93e15e8aea93cfb0b024f
Babich <g8h2.babayan@yandex.ru>
2019-11-22 16:08:44 +0300

    Game create endpoint responses with game uuid now.

--- internal/pkg/game/usecase.go
+++ internal/pkg/game/usecase.go
@@ -9,7 +9,7 @@ import (
 type UseCase interface {
 	GetByID(uuid uuid.UUID) (*models.Game, error)
 	GetGameIDByUserID(userID int) (uuid.UUID, error)
-	Create(g models.Game, u models.User) error
+	Create(g *models.Game, u models.User) error
 	Fetch(page int) (*[]models.Game, error)
 	JoinPlayerToGame(playerID int, gameID uuid.UUID) (*models.Game, error)
 	KickPlayerFromGame(playerID int) error
