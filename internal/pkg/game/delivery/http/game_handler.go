2f6f48f1cbf901af969a18932f0ab2b1364b2636
Babich <g8h2.babayan@yandex.ru>
2019-11-24 23:22:23 +0300

    ws handler returns nil

--- internal/pkg/game/delivery/http/game_handler.go
+++ internal/pkg/game/delivery/http/game_handler.go
@@ -168,5 +168,5 @@ func (gh *handler) ws(ctx echo.Context) error {
 	go conn.RunReceive(userID)
 	go conn.RunSend()
 	log.Info("WS handler reached the end.")
-	return ctx.NoContent(http.StatusOK)
+	return nil
 }
