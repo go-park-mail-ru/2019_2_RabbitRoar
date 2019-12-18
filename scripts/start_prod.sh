3bcae95f0f363fad8630ec4fbab6b2004fc23bd2
Alexey Navarkin <navarka.all@gmail.com>
2019-12-18 16:11:24 +0300

    Fix echo debug output.

--- scripts/start_prod.sh
+++ scripts/start_prod.sh
@@ -4,7 +4,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJ_DIR="$SCRIPT_DIR/.."
 
-echo "Running build $PROJ_DIR."
+echo "Running start_prod $PROJ_DIR."
 
 pushd $PROJ_DIR
 
--- scripts/stop_prod.sh
+++ scripts/stop_prod.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 WORK_DIR="$SCRIPT_DIR/.."
 
-echo "Running start_local in $WORK_DIR."
+echo "Running stop_prod in $WORK_DIR."
 
 pushd $WORK_DIR
 
