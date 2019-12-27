26b132539725176de61083926bcbd0168c2f3fbf
Alexey Navarkin <navarka.all@gmail.com>
2019-12-27 20:30:57 +0300

    Implemented sd client side.

--- internal/pkg/balancer/balancer.go
+++ internal/pkg/balancer/balancer.go
@@ -2,39 +2,47 @@ package balancer
 
 import (
 	"fmt"
+	consulapi "github.com/hashicorp/consul/api"
+	"github.com/op/go-logging"
+	"strconv"
+	"time"
+
 	"google.golang.org/grpc/naming"
 )
 
-type testWatcher struct {
-	// the channel to receives name resolution updates
+var log = logging.MustGetLogger("server")
+
+type Watcher struct {
 	update chan *naming.Update
-	// the side channel to get to know how many updates in a batch
 	side chan int
-	// the channel to notifiy update injector that the update reading is done
 	readDone chan int
 }
 
-func (w *testWatcher) Next() (updates []*naming.Update, err error) {
+func (w *Watcher) Next() (updates []*naming.Update, err error) {
 	n := <-w.side
+
 	if n == 0 {
+		log.Error("w.side is closed")
 		return nil, fmt.Errorf("w.side is closed")
 	}
+
 	for i := 0; i < n; i++ {
 		u := <-w.update
 		if u != nil {
 			updates = append(updates, u)
 		}
 	}
+
 	w.readDone <- 0
+
 	return
 }
 
-func (w *testWatcher) Close() {
+func (w *Watcher) Close() {
 	close(w.side)
 }
 
-// Inject naming resolution updates to the testWatcher.
-func (w *testWatcher) inject(updates []*naming.Update) {
+func (w *Watcher) inject(updates []*naming.Update) {
 	w.side <- len(updates)
 	for _, u := range updates {
 		w.update <- u
@@ -42,13 +50,13 @@ func (w *testWatcher) inject(updates []*naming.Update) {
 	<-w.readDone
 }
 
-type testNameResolver struct {
-	w    *testWatcher
-	addr string
+type NameResolver struct {
+	w    *Watcher
+	Addr string
 }
 
-func (r *testNameResolver) Resolve(target string) (naming.Watcher, error) {
-	r.w = &testWatcher{
+func (r *NameResolver) Resolve(target string) (naming.Watcher, error) {
+	r.w = &Watcher{
 		update:   make(chan *naming.Update, 1),
 		side:     make(chan int, 1),
 		readDone: make(chan int),
@@ -56,10 +64,63 @@ func (r *testNameResolver) Resolve(target string) (naming.Watcher, error) {
 	r.w.side <- 1
 	r.w.update <- &naming.Update{
 		Op:   naming.Add,
-		Addr: r.addr,
+		Addr: r.Addr,
 	}
 	go func() {
 		<-r.w.readDone
 	}()
 	return r.w, nil
+}
+
+func RunOnlineSD(servers []string, nameResolver *NameResolver, consul *consulapi.Client) {
+	currAddrs := make(map[string]struct{}, len(servers))
+	for _, addr := range servers {
+		currAddrs[addr] = struct{}{}
+	}
+
+	ticker := time.Tick(5 * time.Second)
+
+	for range ticker {
+		log.Info("Checking session service online.")
+		health, _, err := consul.Health().Service("session-api", "", false, nil)
+		if err != nil {
+			log.Fatalf("Cant get alive services")
+		}
+
+		newAddrs := make(map[string]struct{}, len(health))
+		for _, item := range health {
+			addr := item.Service.Address +
+				":" + strconv.Itoa(item.Service.Port)
+			newAddrs[addr] = struct{}{}
+		}
+		log.Info("Online:", newAddrs)
+
+		var updates []*naming.Update
+		for addr := range currAddrs {
+			if _, exist := newAddrs[addr]; !exist {
+				updates = append(updates, &naming.Update{
+					Op:   naming.Delete,
+					Addr: addr,
+				})
+				delete(currAddrs, addr)
+				log.Warning("Removing:", addr)
+			}
+		}
+
+		for addr := range newAddrs {
+			if _, exist := currAddrs[addr]; !exist {
+				updates = append(updates, &naming.Update{
+					Op:   naming.Add,
+					Addr: addr,
+				})
+				currAddrs[addr] = struct{}{}
+				log.Info("Adding: ", addr)
+			}
+		}
+
+		if len(updates) > 0 {
+			log.Info("Injecting update:", newAddrs)
+			nameResolver.w.inject(updates)
+		}
+	}
 }
